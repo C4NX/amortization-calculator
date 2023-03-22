307fde9c9152f2c199eeef460efb61415bb7c278
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-22 09:04:00 +0100

    Ajout de l'arrondi du tableau a périodicité annuel

--- src/converters/amortizationToTable.js
+++ src/converters/amortizationToTable.js
@@ -1,28 +1,29 @@
-function round(x) {
-  return +x.toFixed(2);
-}
-
 export default function amortizationToTable(state) {
   const tableData = [];
 
   const t = state.taux / 100;
-  const anuite = round(state.montant * (t / (1 - (1 + t) ** -state.duree)));
+  const anuite = state.montant * (t / (1 - (1 + t) ** -state.duree));
 
   // TODO: add taux mensuel
 
   for (let i = 1; i <= state.duree; i += 1) {
     if (i === 1) {
-      const interest = round(state.montant * t);
-      const amortissement = round(anuite - interest);
-      tableData.push([i, state.montant, interest, amortissement, anuite, round(state.montant - amortissement)]);
+      const interest = state.montant * t;
+      const amortissement = anuite - interest;
+      tableData.push([i, state.montant, interest, amortissement, anuite, state.montant - amortissement]);
     } else {
       const montant = tableData[tableData.length - 1][5];
-      const interest = round(montant * t);
-      const amortissement = round(anuite - interest);
-      tableData.push([i, interest, interest, amortissement, anuite, round(montant - amortissement)]);
+      const interest = montant * t;
+      const amortissement = anuite - interest;
+      tableData.push([i, interest, interest, amortissement, anuite, montant - amortissement]);
     }
   }
-
+  for (let i = 0; i < tableData.length; i += 1) {
+    for (let j = 1; j < tableData[i].length; j += 1) {
+      console.log(tableData[i][j]);
+      tableData[i][j] = Math.round(tableData[i][j] * 100) / 100;
+    }
+  }
   tableData.push(['Total']);
 
   console.log('updated tableData: ', tableData);
