293fc34933c56a02d03c3577dbd7d5c19ca36bf9
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-22 09:46:28 +0100

    Ajout des autres type de périodicité et correction taux

--- src/converters/amortizationToTable.js
+++ src/converters/amortizationToTable.js
@@ -1,12 +1,12 @@
 export default function amortizationToTable(state) {
   const tableData = [];
+  // eslint-disable-next-line no-nested-ternary
+  const periode = state.periodicite === 'mensuel' ? 12 : state.periodicite === 'trimestriel' ? 4 : state.periodicite === 'semestriel' ? 2 : 1;
+  const t = (state.periodicite === 'annuel' ? state.taux / 100 : ((1 + state.taux / 100) ** (1 / periode) - 1));
+  const dureePeriode = state.duree * periode;
+  const anuite = state.montant * (t / (1 - (1 + t) ** -dureePeriode));
 
-  const t = state.taux / 100;
-  const anuite = state.montant * (t / (1 - (1 + t) ** -state.duree));
-
-  // TODO: add taux mensuel
-
-  for (let i = 1; i <= state.duree; i += 1) {
+  for (let i = 1; i <= dureePeriode; i += 1) {
     if (i === 1) {
       const interest = state.montant * t;
       const amortissement = anuite - interest;
@@ -20,13 +20,10 @@ export default function amortizationToTable(state) {
   }
   for (let i = 0; i < tableData.length; i += 1) {
     for (let j = 1; j < tableData[i].length; j += 1) {
-      console.log(tableData[i][j]);
       tableData[i][j] = Math.round(tableData[i][j] * 100) / 100;
     }
   }
   tableData.push(['Total']);
-
-  console.log('updated tableData: ', tableData);
 
   return tableData;
 }
